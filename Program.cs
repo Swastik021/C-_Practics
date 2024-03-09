ad40cfb4dc7a5e0d40ee5dee29e97809fe8131e3
Swastik021 <138189215+Swastik021@users.noreply.github.com>
2024-03-09 21:24:59 +0530

    Programss_Files

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CommonLibrary;
+using CommonLibrary.Practice;
+
+Console.WriteLine("Hello, World!");
+
+Class1 ob = new Class1();
+ob.Demo();
+Console.WriteLine("+++++++============Constructor++++++++++========================="); 
+
+Constructor con1 = new Constructor(2);
+con1.Parentmethod();
+con1.CommonMethod();
+Console.WriteLine(con1.Parentmethod);
+Console.WriteLine(con1.CommonMethod);
+Constructor con2   = new Constructor(12);

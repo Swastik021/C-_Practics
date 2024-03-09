ab543f96cecced04798f604ec737db8aa8eef9d2
Swastik021 <138189215+Swastik021@users.noreply.github.com>
2024-03-09 21:22:31 +0530

    Practics_Uploaded C#

---
+++ C#Basisc/Out_Ref_Keywordcs.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonLibrary.CommonLibrary.C_Basisc
+{
+    internal class Out_Ref_Keywordcs
+    {
+    }
+}
---
+++ OOPS/Inheritance/Inheritance.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonLibrary.CommonLibrary.OOPS.Inheritance
+{
+    internal class Paren_ChildRelationship_Inheritance
+    {
+        //parent child relationship is basically inheritance
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+    public class Parent
+    {
+        public int HouseNumber { get; set; }
+        public string Property { get; set; }
+        public string Address { get; set; }
+
+        public Parent()
+        {
+
+
+        }
+        public string BragAboutTheProperty()
+        {
+            return "See i am so richhhhhhhhh";
+
+        }
+
+    }
+    public class Child : Parent
+    {
+        public string BuildMoreProperty()
+        {
+            HouseNumber = 123;
+            return "";
+        }
+    }
+}
